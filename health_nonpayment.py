856e51776ce3ae9f95491d91c6e9645feedf8a1d
fasthill <kangeun@naver.com>
2021-09-09 13:40:18 +0900

    make new df with healthcare nonpayment list in public data

---
+++ health_nonpayment.py
@@ -0,0 +1,55 @@
+# # 공공데이터 API 호출하기 - 비급여진료비정보서비스
+# * https://www.data.go.kr/
+# * https://www.data.go.kr/dataset/3050988/openapi.do
+#
+# 서비스키 발급은 금방 발급이 되나,
+# 사용은 1시간 ~ 24시간 이후에 사용가능함. 본인의 경우는 하루가 지나서야 사용가능했음.
+# * 공공데이터는 사용 신청 (활용 신청) 은 승인 즉시 사용가능
+#
+
+import requests
+from bs4 import BeautifulSoup as bs
+import xmltodict
+import re
+import pandas as pd
+
+api_key = "4fw71cT9NmHBLiTdlrZJS5BZ6VaGfRZUdpzpDVssN6OImKE8WcKzLlpRqq1HY7C%2F6Y%2BV1M9mngsqOXYLXJ624w%3D%3D"
+URL = 'http://apis.data.go.kr/B551182/nonPaymentDamtInfoService/getNonPaymentItemCodeList?pageNo=1&numOfRows=10&ServiceKey='+api_key
+
+URL_O = 'http://apis.data.go.kr/B551182/nonPaymentDamtInfoService/getNonPaymentItemCodeList'
+page_no = str(1)
+numofrows = str(10)
+PAGE_NO = 'pageNo={}'.format(page_no)
+NUMROWS = 'numOfRows=' + numofrows
+URL_SK = 'serviceKey='+api_key
+
+URL_ALL = URL_O + '?' + PAGE_NO + '&' + NUMROWS + '&' + URL_SK
+URL_ALL1 = URL
+req = requests.get(URL_ALL)
+soup = bs(req.content, 'html.parser')
+# print(soup.prettify())
+# print('*'*100)
+
+def define_columns():
+    payment_class = {}
+    payment_class['divCd1'] = '1차분류코드'
+    payment_class['divCd1Nm'] = '1차분류코드명'
+    payment_class['divCd1Dsc'] = '1차분류코드설명'
+    payment_class['divCd2'] = '2차분류코드'
+    payment_class['divCd2Nm'] = '2차분류코드명'
+    payment_class['divCd2Dsc'] = '2차분류코드설명'
+    payment_class['divCd3'] = '3차분류코드'
+    payment_class['divCd3Nm'] = '3차분류코드명'
+    payment_class['divCd3Dsc'] = '3차분류코드설명'
+    return payment_class
+
+dict = xmltodict.parse(req.content)  # change xml to dict format
+
+nonpay_list = dict['response']['body']['items']['item'] # item를 list형으로 정리
+
+df = pd.DataFrame(nonpay_list)
+
+payment_class = define_columns()  # columns name change
+df.rename(columns=payment_class, inplace=True)  # change columns names with dictionary
+
+print(df)

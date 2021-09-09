95a730fdc309489e3e424a0b0678428c2364a809
fasthill <kangeun@naver.com>
2021-09-09 20:47:49 +0900

    different notation for json res.json()

--- json_read.py
+++ json_read.py
@@ -3,7 +3,10 @@ import json
 import pandas as pd
 
 res = requests.get("http://api.open-notify.org/astros.json")
-json_data = json.loads(res.content)
+
+json_data = res.json()
+# json_data = json.loads(res.content)
+# json_data = json.loads(res.text)
 
 df = pd.DataFrame(json_data['people'])
 print(df)
